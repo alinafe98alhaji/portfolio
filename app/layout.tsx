60f292a0eada9f22e75c3e198798f6a505fc5592
alinafe98alhaji <emmanueldicksonmapulanga@gmail.com>
2025-05-15 23:08:40 +0200

    fixed images error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ParticlesBg from "../components/ParticlesBg";
 import { LazyMotion, domAnimation } from "framer-motion";
-import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import { ArrowDown, Github, Linkedin } from "lucide-react";
 
 export default function HeroSection() {
   return (
